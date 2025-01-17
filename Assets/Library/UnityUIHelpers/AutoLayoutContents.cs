b763b4d86f8195aec18519fc20a9e668ef0362b9
Timofei Aaron Topolja <timothytopoly@gmail.com>
2025-01-17 19:10:54 +0200

    AutoLayoutContents.cs align was added + optimization

--- Assets/Library/UnityUIHelpers/AutoLayoutContents.cs
+++ Assets/Library/UnityUIHelpers/AutoLayoutContents.cs
@@ -1,105 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace Library.UnityUIHelpers
 {
-    [ExecuteAlways]
+    [ExecuteAlways, RequireComponent(typeof(RectTransform))]
     public class AutoLayoutContents : MonoBehaviour
     {
+        private enum Alignment
+        {
+            Left,
+            Right,
+            Center
+        }
+        
         [SerializeField] private float _paddingLeft;
         [SerializeField] private float _paddingRight;
         [SerializeField] private float _paddingTop;
         [SerializeField] private float _spacingHorizontal;
         [SerializeField] private float _spacingVertical;
+        [SerializeField] private Alignment _alignment;
 
-        private const float UPDATE_FREQUENCY = 0.5f;
-        
+        private const float UPDATE_FREQUENCY = 0f;
+
+        private RectTransform _rectTransform;
         private float _lastUpdateTime;
+        private readonly List<List<RectTransform>> _table = new();
 
-        private  void Awake()
-        { 
-            LayoutChildrenObjects(gameObject);
+        private void Awake()
+        {
+            AssignRectTransform();
         }
 
         private void Update()
         {
-            if (Application.isEditor && Time.time > _lastUpdateTime + UPDATE_FREQUENCY)
+            if (!Application.isPlaying && Time.time > _lastUpdateTime + UPDATE_FREQUENCY)
             {
-                LayoutChildrenObjects(gameObject);
-                _lastUpdateTime = Time.time;
+                HandleEditor();
             }
         }
 
-        private void LayoutChildrenObjects(GameObject parent = null)
+        private void HandleEditor()
         {
-            // Default to the current GameObject if no parent is specified.
-            parent ??= gameObject;
+            AssignRectTransform();
+            RefreshLayout();
+            
+            _lastUpdateTime = Time.time;
+        }
 
-            // Ensure the parent has a RectTransform.
-            RectTransform parentRectTransform = parent.GetComponent<RectTransform>();
-            if (!parentRectTransform)
+        private void AssignRectTransform()
+        {
+            _rectTransform ??= GetComponent<RectTransform>();
+        }
+
+        private void RefreshLayout()
+        {
+            _table.Clear();
+
+            LayoutChildrenObjects();
+            ApplyAlignment();
+        }
+
+        private List<RectTransform> GetRectTransformChildren()
+        {
+            List<RectTransform> children = new List<RectTransform>();
+
+            foreach (Transform child in gameObject.transform)
             {
-                Debug.LogWarning("Parent does not have a RectTransform. Layout aborted.");
-                return;
+                if (child.TryGetComponent(out RectTransform rectTransform))
+                    children.Add(rectTransform);
             }
 
-            // Initialize row count and check for children.
-            if (parent.transform.childCount < 1)
-            {
+            return children;
+        }
+        
+        private void LayoutChildrenObjects()
+        {
+            List<RectTransform> rectTransformChildren = GetRectTransformChildren();
+            if (rectTransformChildren.Count == 0)
                 return;
-            }
 
-            // Process the first child.
-            Transform firstChild = parent.transform.GetChild(0);
-            RectTransform firstChildRect = firstChild.GetComponent<RectTransform>();
-            if (!firstChildRect)
-            {
-                Debug.LogWarning("First child does not have a RectTransform. Layout aborted.");
-                return;
-            }
-
+            RectTransform previousRectTransform = rectTransformChildren.First();
             Vector2 currentPosition = new Vector2(
-                -parentRectTransform.rect.width / 2f + firstChildRect.rect.width / 2f + _paddingLeft,
-                parentRectTransform.rect.height / 2f - firstChildRect.rect.height / 2f - _paddingTop
+                -_rectTransform.rect.width / 2f + previousRectTransform.rect.width / 2f + _paddingLeft,
+                _rectTransform.rect.height / 2f - previousRectTransform.rect.height / 2f - _paddingTop
             );
 
-            UIPositionHelper.SetAbsoluteAnchoredCenterPosition(firstChildRect, currentPosition);
+            UIPositionHelper.SetAbsoluteAnchoredCenterPosition(previousRectTransform, currentPosition);
+            List<RectTransform> row = new() {previousRectTransform};
 
-            // Iterate through the rest of the children.
-            RectTransform previousRectTransform = firstChildRect;
-            for (int i = 1; i < parent.transform.childCount; i++)
+            for (int i = 1; i < gameObject.transform.childCount; i++)
             {
-                Transform child = parent.transform.GetChild(i);
-                RectTransform currentRectTransform = child.GetComponent<RectTransform>();
-
-                if (!currentRectTransform)
-                {
-                    Debug.LogWarning($"Child at index {i} does not have a RectTransform. Skipping.");
+                if (!gameObject.transform.GetChild(i).TryGetComponent(out RectTransform currentRectTransform))
                     continue;
-                }
 
-                // Calculate offsets and positions.
                 float offsetX = previousRectTransform.rect.width / 2f + currentRectTransform.rect.width / 2f + _spacingHorizontal;
 
-                // Check for horizontal overflow.
-                if (currentPosition.x + offsetX + currentRectTransform.rect.width / 2f + _paddingRight > parentRectTransform.rect.width / 2f)
+                if (currentPosition.x + offsetX + currentRectTransform.rect.width / 2f + _paddingRight > _rectTransform.rect.width / 2f)
                 {
                     var offsetY = -previousRectTransform.rect.height - _spacingVertical;
                     currentPosition = new Vector2(
-                        -parentRectTransform.rect.width / 2f + currentRectTransform.rect.width / 2f + _paddingLeft,
+                        -_rectTransform.rect.width / 2f + currentRectTransform.rect.width / 2f + _paddingLeft,
                         currentPosition.y + offsetY
                     );
+                    
+                    _table.Add(row);
+                    row = new() {currentRectTransform};
                 }
                 else
                 {
+                    row.Add(currentRectTransform);
                     currentPosition.x += offsetX;
                 }
-
+                
                 UIPositionHelper.SetAbsoluteAnchoredCenterPosition(currentRectTransform, currentPosition);
+                
                 previousRectTransform = currentRectTransform;
             }
             
-            return;
+            _table.Add(row);
+        }
+
+        private void ApplyAlignment()
+        {
+            foreach (var row in _table)
+            {
+                foreach (RectTransform rectTransform in row)
+                {
+                    float borderX = GetRectHorizontalBorder(row.LastOrDefault());
+                    float offsetX = GetAlignmentOffset(borderX);
+                    
+                    Vector2 targetPosition = rectTransform.anchoredPosition + Vector2.right * offsetX;
+                    UIPositionHelper.SetAnchoredCenterPosition(rectTransform, targetPosition);
+                }
+            }
+        }
+
+        private float GetAlignmentOffset(float borderX)
+        {
+            switch (_alignment)
+            {
+                case Alignment.Right:
+                    return _rectTransform.rect.width - borderX + _paddingRight;
+                case Alignment.Center:
+                    return (_rectTransform.rect.width - borderX) / 2;
+                default:
+                    return 0;
+            }
+        }
+
+        private float GetRectHorizontalBorder(RectTransform rectTransform)
+        {
+            return rectTransform.anchoredPosition.x + rectTransform.rect.width / 2;
         }
     }
 }
